9e5ce414dd66a96f68764f08e60f213deeca8f65
auroraam <auroramaisyah2005@mail.ugm.ac.id>
2025-03-27 16:02:01 +0700

    feat: connection to db

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3500;
+const mongoose = require('mongoose');
+const connectDB = require('./config/db');
+connectDB();
 
 app.get("/", (req, res) => {
   res.json({ message: "Landing Page" });
 });
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from Express.js backend!" });
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  server = app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+    console.log(`Environment: ${process.env.NODE_ENV}`);
+  });
 });
 
-// Menjalankan server
-app.listen(PORT, () => {
-  console.log(`🚀 Backend berjalan di http://localhost:${PORT}`);
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    'mongoErrLog.log');
 });
