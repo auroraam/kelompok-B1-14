ab2b9155f4dd315aec49b0527e6ae5281b6cfdfd
auroraam <auroramaisyah2005@mail.ugm.ac.id>
2025-04-24 19:00:48 +0700

    feat: model

---
+++ backend/model/Notification.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const User = require('./User');
+const Task = require('./Task');
+
+const notificationSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    taskId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'task',
+        required: true
+    },
+    notificationTime: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['sent', 'pending', 'read'],
+        default: 'pending'
+    }
+});
+
+module.exports = mongoose.model('Notification', notificationSchema);
---
+++ backend/model/Setting.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const User = require('./User');
+
+const settingsSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true,
+        unique: true
+    },
+    themeMode: {
+        type: String,
+        enum: ['light', 'dark'],
+        default: 'light'
+    },
+    notificationPreference: {
+        type: String,
+        enum: ['on', 'off'],
+        default: 'on'
+    }
+});
+
+module.exports = mongoose.model('Settings', settingsSchema);
